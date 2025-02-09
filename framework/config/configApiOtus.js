e997fcc5f3aa8faa3c10f9dceb91901124da3eed
QADrowning <olga.a.sidorenko@mail.ru>
2025-02-09 19:29:35 +0300

    Проектная работа

--- framework/config/configApiOtus.js
+++
@@ -1,9 +0,0 @@
-// import 'dotenv/config'
-
-// export default Object.freeze({
-//   baseURL: process.env.TEST_BOOKSTORE_API_URL ?? 'https://bookstore.demoqa.com',
-//   username: process.env.TEST_BOOKSTORE_USERNAME ?? 'OlgaOtus',
-//   password: process.env.TEST_BOOKSTORE_PASSWORD ?? 'Password31!',
-//   usernameFake: process.env.TEST_BOOKSTORE_USERNAME_FAKE ?? 'Olga',
-//   passwordFake: process.env.TEST_BOOKSTORE_PASSWORD_FALE ?? 'fake',
-// })
--- framework/config/configRWA.js
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config'
-
-export default Object.freeze({
-  baseURL:
-    process.env.TEST_RWA_BASE_URL ?? 'https://rwa-test/',
-  username: process.env.TEST_RWA_USERNAME ?? 'test',
-  email: process.env.TEST_RWA_EMAIL ?? 'test@mail.ru',
-  password: process.env.TEST_RWA_PASSWORD ?? 'P@ssw0rd',
-})
